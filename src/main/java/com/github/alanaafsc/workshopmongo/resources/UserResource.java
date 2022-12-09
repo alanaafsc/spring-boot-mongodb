d43b790751fbd93d348f2f797ac083a7839e8474
alanaafsc <alana.ingrid@ufpe.br>
2022-12-09 17:11:26 -0300

    Endpoint to return a user's posts

--- src/main/java/com/github/alanaafsc/workshopmongo/resources/UserResource.java
+++ src/main/java/com/github/alanaafsc/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.github.alanaafsc.workshopmongo.domain.Post;
 import com.github.alanaafsc.workshopmongo.domain.User;
 import com.github.alanaafsc.workshopmongo.dto.UserDTO;
 import com.github.alanaafsc.workshopmongo.services.UserService;
@@ -63,5 +64,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
